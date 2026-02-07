0a3d798e0d0fea0955deb71a264aac880e52730f
Umberto Francesco Carolini <umbertocarolini4112@gmail.com>
2026-02-07 23:09:34 +0100

    test script fix

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import gymnasium as gym
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3 import SAC, PPO, TD3, A2C, DDPG
 from src.env.wrappers.observations import ObservationWrapper
-from src.env.self_balancing_robot_env.self_balancing_robot_env import (SelfBalancingRobotEnv)
+from src.env.robot import SelfBalancingRobotEnv
 from src.utils.files import compress_and_remove
 from src.utils.parser import parse_test_arguments, parse_model
 
@@ -19,37 +19,43 @@ def make_env(environment_path="./models/scene.xml", max_time=float("inf")):
     """
     Crea un'istanza dell'ambiente SelfBalancingRobotEnv con rendering.
     """
-    env = SelfBalancingRobotEnv(environment_path=environment_path, max_time=max_time)
+    env = SelfBalancingRobotEnv(
+        environment_path=environment_path, max_time=max_time)
     env = ObservationWrapper(env)
     env = Monitor(env)
     return env
+
 
 def read_joystick_input(joystick):
     rpt = joystick.read(64)
     if rpt:
         lx, ly, rx, ry = map(normalize, rpt[1:5])
-        return np.array([lx, ly, rx, ry], dtype=np.float32)  # Return as a numpy array
+        # Return as a numpy array
+        return np.array([lx, ly, rx, ry], dtype=np.float32)
     return None  # Return None if no input is read
+
 
 def extract(folder_path: str) -> str:
     """
     Decompress a .tar.gz folder.
-    
+
     Return: path to the decompressed folder.
     """
     # Check if the folder_path is already decompressed
     if os.path.exists(folder_path):
         return folder_path
-    
-    if (os.path.exists(f"{folder_path}.tar.gz") or \
-        os.path.exists(f"{folder_path}.tgz") or \
-        os.path.exists(f"{folder_path}.tar") or \
-        os.path.exists(f"{folder_path}.zip") or \
-        os.path.exists(f"{folder_path}.gz")):
+
+    if (os.path.exists(f"{folder_path}.tar.gz") or
+        os.path.exists(f"{folder_path}.tgz") or
+        os.path.exists(f"{folder_path}.tar") or
+        os.path.exists(f"{folder_path}.zip") or
+            os.path.exists(f"{folder_path}.gz")):
         with tarfile.open(f"{folder_path}.tar.gz", "r:gz") as tar:
             tar.extractall(path=os.path.dirname(folder_path))
-        print(f"Decompressed folder: {os.path.splitext(os.path.splitext(folder_path)[0])[0]}")
+        print(
+            f"Decompressed folder: {os.path.splitext(os.path.splitext(folder_path)[0])[0]}")
         return os.path.splitext(os.path.splitext(folder_path)[0])[0]
+
 
 if __name__ == "__main__":
     # Parse degli argomenti della riga di comando
@@ -58,24 +64,28 @@ if __name__ == "__main__":
     MAX_TIME = args.max_time
     STEPS = args.test_steps
     INTERACTIVE = args.interactive
-    
+
     # Load the json configuration
     if POLICY is None:
-        raise ValueError("Please provide the path to the model using --path argument.")
+        raise ValueError(
+            "Please provide the path to the model using --path argument.")
     compressed_path = f"./policies/{POLICY}"
-    
-    POLICY_FOLDER_PATH = extract(compressed_path) # policies/POLICY
-    CONFIG_PATH = f"{POLICY_FOLDER_PATH}/config.json" # policies/POLICY/config.json
-    POLICY_PATH = f"{POLICY_FOLDER_PATH}/policy" # policies/POLICY/policy
-    ENV_PATH = f"{POLICY_FOLDER_PATH}/scene.xml" # policies/POLICY/scene.xml
-    
+
+    POLICY_FOLDER_PATH = extract(compressed_path)  # policies/POLICY
+    # policies/POLICY/config.json
+    CONFIG_PATH = f"{POLICY_FOLDER_PATH}/config.json"
+    POLICY_PATH = f"{POLICY_FOLDER_PATH}/policy"  # policies/POLICY/policy
+    ENV_PATH = f"{POLICY_FOLDER_PATH}/scene.xml"  # policies/POLICY/scene.xml
+
     if not os.path.exists(ENV_PATH):
         ENV_PATH = "./models/scene.xml"
     if not os.path.exists(CONFIG_PATH) and not os.path.exists(f"{POLICY_FOLDER_PATH}/{POLICY}.json"):
-        raise FileNotFoundError(f"Configuration json file does not exist in {POLICY_FOLDER_PATH}.")
+        raise FileNotFoundError(
+            f"Configuration json file does not exist in {POLICY_FOLDER_PATH}.")
     if not os.path.exists(f"{POLICY_PATH}.zip") and not os.path.exists(f"{POLICY_FOLDER_PATH}/{POLICY}.zip"):
-        raise FileNotFoundError(f"Model file policy.zip does not exist in {POLICY_FOLDER_PATH}.")
-    
+        raise FileNotFoundError(
+            f"Model file policy.zip does not exist in {POLICY_FOLDER_PATH}.")
+
     # Rename the files with the standard names if necessary
     if not os.path.exists(POLICY_PATH) and os.path.exists(f"{POLICY_FOLDER_PATH}/{POLICY}.zip"):
         os.rename(f"{POLICY_FOLDER_PATH}/{POLICY}.zip", f"{POLICY_PATH}.zip")
@@ -104,7 +114,6 @@ if __name__ == "__main__":
     # Get the file from the path
     with open(CONFIG_PATH, "r") as f:
         config = json.load(f)
-   
 
     print("Configuration loaded:")
     print(json.dumps(config, indent=4))
@@ -114,7 +123,7 @@ if __name__ == "__main__":
     env = make_env(environment_path=ENV_PATH, max_time=MAX_TIME)
 
     model = MODEL.load(POLICY_PATH, env=env)
-    print(f"Loaded model: { POLICY_PATH} ")
+    print(f"Loaded model: {POLICY_PATH} ")
 
     obs, _ = env.reset()
     for _ in range(args.test_steps):
@@ -127,6 +136,5 @@ if __name__ == "__main__":
             break
         if terminated or truncated:
             obs, _ = env.reset()
-    
+
     compress_and_remove(POLICY_FOLDER_PATH)
-    
